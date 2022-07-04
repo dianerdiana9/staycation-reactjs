cf2d6690ad6e5693d864cf5898088a6a3568e2e3
dianerdiana9 <dianerdiana.de@gmail.com>
2022-07-05 03:06:27 +0700

    first commit

---
+++ src/views/100ProjectChallenge/Day1/index.js
@@ -0,0 +1,12 @@
+import { Container, Button } from '@mui/material'
+import React from 'react'
+
+function RandomMealGenerator() {
+  return (
+    <Container>
+      <Button variant="contained">Berbeda</Button>
+    </Container>
+  )
+}
+
+export default RandomMealGenerator
