6216d08eb36f196e101afc3614e84a7f3dea160f
dianerdiana9 <dianerdiana.de@gmail.com>
2022-07-10 10:19:22 +0700

    add fading transition in each section

--- src/components/MostPicked.js
+++ src/components/MostPicked.js
@@ -3,46 +3,56 @@ import React from 'react'
 // ** custom component
 import Button from '@components/Button'
 
+// fading components
+import Fade from 'react-reveal/Fade'
+
 export default function MostPicked(props) {
   return (
     <section className="container mt-5 pt-4 pb-5" ref={props.refMostPicked}>
-      <h4 className="mb-3">Most Picked</h4>
-      <div className="container-grid">
-        {props.data.map((item, index) => {
-          return (
-            <div
-              className={`item column-4 ${index === 0 ? 'row-2' : 'row-1'}`}
-              key={`mostpick-${index}`}
-            >
-              <div className="card card-featured">
-                <div className="tag">
-                  ${item.price}
-                  <span className="font-weight-light"> per {item.unit}</span>
+      <Fade bottom>
+        <h4 className="mb-3">Most Picked</h4>
+        <div className="container-grid">
+          {props.data.map((item, index) => {
+            return (
+              <Fade bottom delay={500 * index}>
+                <div
+                  className={`item column-4 ${index === 0 ? 'row-2' : 'row-1'}`}
+                  key={`mostpick-${index}`}
+                >
+                  <div className="card card-featured">
+                    <div className="tag">
+                      ${item.price}
+                      <span className="font-weight-light">
+                        {' '}
+                        per {item.unit}
+                      </span>
+                    </div>
+                    <figure className="img-wrapper">
+                      <img
+                        src={item.imageUrl}
+                        alt={item.name}
+                        className="img-cover"
+                      />
+                    </figure>
+                    <div className="meta-wrapper">
+                      <Button
+                        className="stretched-link d-block text-white"
+                        type="link"
+                        href={`/properties/${item._id}`}
+                      >
+                        <h5>{item.name}</h5>
+                      </Button>
+                      <span>
+                        {item.city}, {item.country}
+                      </span>
+                    </div>
+                  </div>
                 </div>
-                <figure className="img-wrapper">
-                  <img
-                    src={item.imageUrl}
-                    alt={item.name}
-                    className="img-cover"
-                  />
-                </figure>
-                <div className="meta-wrapper">
-                  <Button
-                    className="stretched-link d-block text-white"
-                    type="link"
-                    href={`/properties/${item._id}`}
-                  >
-                    <h5>{item.name}</h5>
-                  </Button>
-                  <span>
-                    {item.city}, {item.country}
-                  </span>
-                </div>
-              </div>
-            </div>
-          )
-        })}
-      </div>
+              </Fade>
+            )
+          })}
+        </div>
+      </Fade>
     </section>
   )
 }
